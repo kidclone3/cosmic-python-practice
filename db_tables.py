648b5177f8ee2ce3106ec5a2aa17d597f5b16ea6
delus <kid.clone.3@gmail.com>
2025-05-14 12:09:26 +0700

    replace the rawsql with orm and start_mappers

--- db_tables.py
+++ db_tables.py
@@ -1,6 +1,7 @@
-from enum import unique
-
 from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import mapper, relationship
+
+import model
 
 metadata = MetaData()
 
@@ -10,14 +11,14 @@ order_lines = Table(
     Column("id", Integer, primary_key=True, autoincrement=True),
     Column("sku", String(255)),
     Column("qty", Integer, nullable=False),
-    Column("orderid", String(255), unique=True),
+    Column("orderid", String(255)),
 )
 
 batches = Table(
     "batches",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("reference", String(255), unique=True),
+    Column("reference", String(255)),
     Column("sku", String(255)),
     Column("_purchased_quantity", Integer, nullable=False),
     Column("eta", Date, nullable=True),
@@ -30,3 +31,14 @@ allocations = Table(
     Column("orderline_id", ForeignKey("order_lines.id")),
     Column("batch_id", ForeignKey("batches.id")),
 )
+def start_mappers():
+    lines_mapper = mapper(model.OrderLine, order_lines)
+    mapper(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set,
+            )
+        },
+    )
